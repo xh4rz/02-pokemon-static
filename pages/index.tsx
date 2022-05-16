eb00c4d8318e7c9414de77e2465c2a677b130c5c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-15 21:53:36 -0500

    50. Componente para las tarjetas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Card, Grid, Row, Text } from '@nextui-org/react';
+import { Grid } from '@nextui-org/react';
 import { NextPage, GetStaticProps } from 'next';
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
+import { PokemonCard } from '../components/pokemon';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
 interface Props {
@@ -12,20 +13,8 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
 	return (
 		<Layout title="Listado de Pokémons">
 			<Grid.Container gap={2} justify="flex-start">
-				{pokemons.map(({ id, name, img }) => (
-					<Grid xs={6} sm={3} md={2} xl={1} key={id}>
-						<Card hoverable clickable>
-							<Card.Body css={{ p: 1 }}>
-								<Card.Image src={img} width="100%" height={140} />
-							</Card.Body>
-							<Card.Footer>
-								<Row justify="space-between">
-									<Text transform="capitalize">{name}</Text>
-									<Text>#{id}</Text>
-								</Row>
-							</Card.Footer>
-						</Card>
-					</Grid>
+				{pokemons.map((pokemon) => (
+					<PokemonCard key={pokemon.id} pokemon={pokemon} />
 				))}
 			</Grid.Container>
 		</Layout>
