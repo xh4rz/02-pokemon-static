08c310dcfcf115311ac2c46a8640cdcf91dea74c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-15 20:37:42 -0500

    47. Colocar el tipo de respuesta de Axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage, GetStaticProps } from 'next';
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
+import { PokemonListResponse } from '../interfaces';
 
 const HomePage: NextPage = (props) => {
-	console.log({ props });
+	console.log(props);
 	return (
 		<Layout title="Listado de Pokémons">
 			<ul>
@@ -20,7 +21,7 @@ const HomePage: NextPage = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-	const { data } = await pokeApi.get('/pokemon?limit=151');
+	const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
 
 	return {
 		props: {
