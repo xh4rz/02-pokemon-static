ae2d605930ce9ffd9e2de87720e657d6e561ca81
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-21 22:47:38 -0500

    68. Librerías externas - Canvas-Confetti

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
+import confetti from 'canvas-confetti';
 import { Layout } from '../../components/layouts';
 import { pokeApi } from '../../api';
 import { Pokemon } from '../../interfaces';
@@ -16,6 +17,19 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 	const onToggleFavorite = () => {
 		localFavorites.toggleFavorite(pokemon.id);
 		setIsInFavorites(!isInFavorites);
+
+		if (isInFavorites) return;
+
+		confetti({
+			zIndex: 999,
+			particleCount: 100,
+			spread: 160,
+			angle: -100,
+			origin: {
+				x: 1,
+				y: 0
+			}
+		});
 	};
 
 	useEffect(() => {
