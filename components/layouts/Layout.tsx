e6c628597e249ab6dcacbf8c8c07415cb1c41f5b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-22 14:49:56 -0500

    73. Open Graph Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,8 @@ interface Props {
 	title?: string;
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
 	return (
 		<>
@@ -18,6 +20,13 @@ export const Layout: FC<Props> = ({ children, title }) => {
 					content={`Información sobre el pokémon ${title}`}
 				/>
 				<meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+				<meta property="og:title" content={`Información sobre ${title}`} />
+				<meta
+					property="og:description"
+					content={`Esta es la página sobre ${title}`}
+				/>
+				<meta property="og:image" content={`${origin}/img/banner.png`} />
 			</Head>
 
 			<Navbar />
