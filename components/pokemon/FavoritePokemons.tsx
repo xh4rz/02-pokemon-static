33458bb8e10ae209d98b252453cccca99fcc654f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-21 22:36:10 -0500

    67. Tarea - Mejorar la lectura de nuestra página

---
+++ components/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@nextui-org/react';
+import { FavoriteCardPokemon } from './';
+
+interface Props {
+	pokemons: number[];
+}
+
+export const FavoritePokemons = ({ pokemons }: Props) => {
+	return (
+		<Grid.Container gap={2} direction="row" justify="flex-start">
+			{pokemons.map((id) => (
+				<FavoriteCardPokemon key={id} pokemonId={id} />
+			))}
+		</Grid.Container>
+	);
+};
